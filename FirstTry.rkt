919cf3a8243414f8de4fa8794fe8a91489eb5e17
Edgar Daniel Rojas Vazquez <edgardanielprog@gmail.com>
2018-10-07 03:08:35 -0500

    Created second function that iterates n times the bubble up function

--- FirstTry.rkt
+++ FirstTry.rkt
@@ -4,15 +4,23 @@
 
 ;; Bubble sort in Scheme
 
+;; This function returns the list with the biggest value to the top
 (define (bubblesort lista) "Bubble sort program scheme"
    (cond ((null? (cdr lista)) lista)
          ((> (car lista) (cadr lista)) 
            (cons (cadr lista) (bubblesort (cons (car lista) (cdr (cdr lista)))))
          )
-         (else (cons( (car lista) (bubblesort (cdr lista))))
+         (else (cons (car lista) (bubblesort (cdr lista)))
          )
    )
 )
 
-(list 1 2 3 4 5)
-(bubblesort (list 4 3 2 1))
+(define (actualsort listb size)
+  (cond ((eq? size 1) (bubblesort listb))
+        (else (actualsort (bubblesort listb) (- size 1)))
+        )
+)
+
+;;(list 1 2 3 4 5)
+;;(bubblesort (list 4 3 2 1))
+(actualsort (list 4 3 2 1) (length (list 4 3 2 1)))
